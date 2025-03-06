97f9a2a829730a52a79a07dac96a3dc9ff5c3b6d
jvvaccari <jv_vaccari@live.com>
2025-03-06 14:25:47 -0300

    Adjusting the GetUserClassService

--- src/services/UserClassServices/GetUserClassService.ts
+++ src/services/UserClassServices/GetUserClassService.ts
@@ -1,23 +1,15 @@
 import prisma from "../../prisma";
 
 export class GetUserClassService {
-  async execute(classId?: string, userId?: string) {
+  async execute(userId?: string) {
     try {
-      if (classId) {
+      console.log(userId);
+      if (userId) {
         const classes = await prisma.userClass.findMany({
           where: { userId: userId },
           include: { class: true },
         });
         return { classes: classes.map((c) => c.class) };
-      }
-
-      if (userId) {
-        const students = await prisma.userClass.findMany({
-          where: { classId: classId },
-          include: { user: true },
-        });
-
-        return { students: students.map((s) => s.user) };
       }
 
       throw new Error("É necessário fornecer um userId ou classId.");
