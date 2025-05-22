527dff1a9e2f5748d14d7a14d52bf4510a411658
João Vaccari <jvvaccari1@gmail.com>
2025-05-21 23:13:26 -0300

    refatora server.ts para melhorar a formatação do código e a legibilidade

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,68 +14,75 @@ import { userClassRoutes } from "../routes/userClass.routes";
 import { awsRoutes } from "../routes/aws.routes";
 import { frequencyList } from "../routes/frequencyList.routes";
 
-
 dotenv.config();
 
 const PORT = process.env.PORT || 5722;
-const SECRET_KEY = process.env.JWT || "c9bd4601dd9f791eedf663b0eec348cbad4578b1c70cfeaeeaf38e087533693f" 
+const SECRET_KEY =
+  process.env.JWT ||
+  "c9bd4601dd9f791eedf663b0eec348cbad4578b1c70cfeaeeaf38e087533693f";
 
 const app = Fastify({
-  logger: process.env.NODE_ENV === "development"
-    ? pino({
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: "HH:MM:ss Z",
-            ignore: "pid,hostname",
-            messageFormat: "{req.method} {req.url} {res.statusCode}"
-          }
-        }
-      })
-    : true, 
+  logger:
+    process.env.NODE_ENV === "development"
+      ? pino({
+          transport: {
+            target: "pino-pretty",
+            options: {
+              colorize: true,
+              translateTime: "HH:MM:ss Z",
+              ignore: "pid,hostname",
+              messageFormat: "{req.method} {req.url} {res.statusCode}",
+            },
+          },
+        })
+      : true,
 });
 
 // Hook para logar requisições
 app.addHook("onRequest", (request, reply, done) => {
-  request.log.info({ method: request.method, url: request.url }, "Request received");
+  request.log.info(
+    { method: request.method, url: request.url },
+    "Request received"
+  );
   done();
 });
 
 // Hook para logar respostas
 app.addHook("onResponse", (request, reply, done) => {
-  request.log.info({ method: request.method, url: request.url, statusCode: reply.statusCode }, "Response sent");
+  request.log.info(
+    { method: request.method, url: request.url, statusCode: reply.statusCode },
+    "Response sent"
+  );
   done();
 });
 
-
 async function start() {
-  await app.register(jwt,{secret: SECRET_KEY});
-
+  await app.register(jwt, { secret: SECRET_KEY });
 
   app.decorate("authenticate", async (req: any, res: any) => {
     try {
       await req.jwtVerify();
-      console.log("Autenticado com sucesso:", req.user);  
+      console.log("Autenticado com sucesso:", req.user);
     } catch (err) {
       console.log("Erro na autenticação:", err);
       return res.status(401).send({ error: "Unauthorized/expired token" });
     }
   });
-  
-  
-  
+
   app.decorate("isAdmin", async (req: any, res: any) => {
-    console.log("Role",req.user.role);
+    console.log("Role", req.user.role);
     if (req.user.role !== "admin") {
       return res.status(403).send({ error: "Access denied. Admins only." });
     }
   });
-  
 
   await app.register(cors);
   await app.register(fastifyFormbody);
-  await app.register(FastifyMultipart)
+  await app.register(FastifyMultipart, {
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  });
 
   await app.register(classRoutes);
   await app.register(lessonRoutes);
@@ -86,7 +93,7 @@ async function start() {
   await app.register(frequencyList);
 
   try {
-    await app.listen({ port: Number(PORT), host: "0.0.0.0"  });
+    await app.listen({ port: Number(PORT), host: "0.0.0.0" });
     console.log(`Server is running on ${PORT}`);
   } catch (error) {
     app.log.error(error);
@@ -100,4 +107,4 @@ app.get("/", async (request, reply) => {
 
 start();
 
-export {app}
+export { app };
